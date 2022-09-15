b8834f884d0afe1cdabb1b33164a0adf3dc6a398
Walter, Mona <mona.walter@tecalliance.net>
2022-09-15 09:09:18 +0200

    Added new project

---
+++ Begin_CSharp/Begin_CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Mona";
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
