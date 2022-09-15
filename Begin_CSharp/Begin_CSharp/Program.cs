855801a54b3d02ae0ebf9c105177c556a7f916ee
Walter, Mona <mona.walter@tecalliance.net>
2022-09-15 09:32:28 +0200

    Created calculate user age

--- Begin_CSharp/Begin_CSharp/Program.cs
+++ Begin_CSharp/Begin_CSharp/Program.cs
@@ -14,6 +14,17 @@ namespace Begin_CSharp
             string userName = Console.ReadLine();
             //string userName = "Mona";
             Console.WriteLine("Hello " + userName);
+
+            Console.WriteLine("Wie alt bist aktuell?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            Console.WriteLine("Wie alt bist in X Jahren?");
+            int nextYear = Convert.ToInt32(Console.ReadLine());
+            Console.ReadLine();
+
+            int sumYears = age + nextYear;
+            Console.WriteLine("Du bist also in " + nextYear + " Jahren, " + sumYears);
             Console.ReadLine();
         }
     }
